3b53871fbc5bd7e975457f8b15302943a60bd246
Siddharth <2004sid@gmail.com>
2023-07-28 22:31:50 +0530

    agent script

--- gameplay/agent.py
+++ gameplay/agent.py
@@ -10,4 +10,35 @@ LR = 0.001
 
 class Agent:
     def __init__(self) -> None:
+        self.n_games = 0
+        self.epsilon = 0
+        self.gamma = 0
+        self.memory = deque(maxlen=MAX_MEMORY)
+
+
+    def get_state(self, game):
         pass
+
+    def remember(self, state, action, reward, next_state, done):
+        pass
+
+    def train_long_memory(self):
+        pass
+
+    def train_short_memory(self):
+        pass
+
+    def get_action(self, state):
+        pass
+
+
+def train():
+    plot_scores = []
+    mean_scores = []
+    total_scores = 0
+    best_score = 0
+    agent = Agent()
+    game = game
+
+if __name__ == '__main__':
+    train()
