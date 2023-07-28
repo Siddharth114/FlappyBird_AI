fc5c9714edaac50e00b9c236d933518ddefbeec5
Siddharth <2004sid@gmail.com>
2023-07-28 09:32:16 +0530

    fixed collision logic

--- gameplay/main.py
+++ gameplay/main.py
@@ -2,19 +2,21 @@ import pygame
 from pygame.locals import *
 import random
 import sys
+import math
 
 FPS = 32
-SCREEN_WIDTH = 289
-SCREEN_HEIGHT = 511
+SCREEN_WIDTH = 336
+SCREEN_HEIGHT = 600
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 GROUND_Y = SCREEN_HEIGHT
 PIPE_WIDTH = 20
 PIPE_HEIGHT = 500
+PLAYER_RADIUS = 10
 
 
 def startScreen():
     player_x = int(SCREEN_WIDTH / 5)
-    player_y = int(SCREEN_HEIGHT - 10)/2
+    player_y = int(SCREEN_HEIGHT - PLAYER_RADIUS) / 2
     while True:
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
@@ -23,8 +25,10 @@ def startScreen():
             elif event.type == KEYDOWN and event.key == K_SPACE:
                 return
             else:
-                SCREEN.fill([0,0,0])
-                pygame.draw.circle(SCREEN, (255, 255, 255), (player_x, player_y), 10)
+                SCREEN.fill([0, 0, 0])
+                pygame.draw.circle(
+                    SCREEN, (255, 255, 255), (player_x, player_y), PLAYER_RADIUS
+                )
                 pygame.display.update()
                 FPSCLOCK.tick(FPS)
 
@@ -71,74 +75,98 @@ def mainGame():
 
         player_mid_pos = player_x + 5
         for pipe in upper_pipes:
-            pipe_mid_pos = pipe['x'] + PIPE_WIDTH/2
-            if pipe_mid_pos<=player_mid_pos<pipe_mid_pos+4:
-                score+=1
+            pipe_mid_pos = pipe["x"] + PIPE_WIDTH / 2
+            if pipe_mid_pos <= player_mid_pos < pipe_mid_pos + 4:
+                score += 1
 
         if player_velocity_y < player_max_velocity_y and not player_flapped:
             player_velocity_y += player_accelaration_y
-        
+
         if player_flapped:
-            player_flapped=False
+            player_flapped = False
 
         player_height = 5
 
         player_y += min(player_velocity_y, GROUND_Y - player_y - player_height)
 
         for upper_pipe, lower_pipe in zip(upper_pipes, lower_pipes):
-            upper_pipe['x'] += pipe_velocity_x
-            lower_pipe['x'] += pipe_velocity_x
+            upper_pipe["x"] += pipe_velocity_x
+            lower_pipe["x"] += pipe_velocity_x
 
-        if 0<upper_pipes[0]['x']<5:
+        if 0 < upper_pipes[0]["x"] < 5:
             new_pipe = getRandomPipe()
             upper_pipes.append(new_pipe[0])
             lower_pipes.append(new_pipe[1])
 
-        if upper_pipes[0]['x'] < -PIPE_WIDTH:
+        if upper_pipes[0]["x"] < -PIPE_WIDTH:
             upper_pipes.pop(0)
             lower_pipes.pop(0)
 
-        SCREEN.fill([0,0,0])
+        SCREEN.fill([0, 0, 0])
         for upper_pipe, lower_pipe in zip(upper_pipes, lower_pipes):
-            pygame.draw.rect(SCREEN, (255,255,255), pygame.Rect(upper_pipe['x'], upper_pipe['y'], PIPE_WIDTH, PIPE_HEIGHT))
-            pygame.draw.rect(SCREEN, (255,255,255), pygame.Rect(lower_pipe['x'], lower_pipe['y'], PIPE_WIDTH, PIPE_HEIGHT))
+            pygame.draw.rect(
+                SCREEN,
+                (255, 255, 255),
+                pygame.Rect(upper_pipe["x"], upper_pipe["y"], PIPE_WIDTH, PIPE_HEIGHT),
+            )
+            pygame.draw.rect(
+                SCREEN,
+                (255, 255, 255),
+                pygame.Rect(lower_pipe["x"], lower_pipe["y"], PIPE_WIDTH, PIPE_HEIGHT),
+            )
         pygame.draw.circle(SCREEN, (255, 255, 255), (player_x, player_y), 10)
         score_text = my_font.render(str(score), False, (255, 255, 255))
-        SCREEN.blit(score_text, (SCREEN_WIDTH/2, 20))
+        SCREEN.blit(score_text, (SCREEN_WIDTH / 2, 20))
 
         pygame.display.update()
         FPSCLOCK.tick(FPS)
 
 
 def isCollide(player_x, player_y, upper_pipes, lower_pipes):
-    if player_y > GROUND_Y - 10 or player_y < 0:
+    if player_y >= GROUND_Y - PLAYER_RADIUS or player_y - PLAYER_RADIUS <= 0:
         return True
-    
+
     for pipe in upper_pipes:
-        if player_y<(PIPE_HEIGHT + pipe['y']) and abs(player_x-pipe['x'])<PIPE_WIDTH:
+        bottom_y = pipe['y'] + PIPE_HEIGHT
+        left_x = pipe['x']
+        right_x = left_x + PIPE_WIDTH
+
+        closest_x = max(pipe['x'], min(player_x, pipe['x'] + PIPE_WIDTH))
+        closest_y = max(pipe['y'], min(player_y, pipe['y'] + PIPE_HEIGHT))
+
+        distance = math.sqrt((player_x - closest_x) ** 2 + (player_y - closest_y) ** 2)
+
+        if distance <= PLAYER_RADIUS:
             return True
-        
+
     for pipe in lower_pipes:
-        if player_y+10 > pipe['y'] and abs(player_x-pipe['x'])<PIPE_WIDTH:
+        bottom_y = pipe['y'] + PIPE_HEIGHT
+        left_x = pipe['x']
+        right_x = left_x + PIPE_WIDTH
+
+        closest_x = max(pipe['x'], min(player_x, pipe['x'] + PIPE_WIDTH))
+        closest_y = max(pipe['y'], min(player_y, pipe['y'] + PIPE_HEIGHT))
+
+        distance = math.sqrt((player_x - closest_x) ** 2 + (player_y - closest_y) ** 2)
+
+        if distance <= PLAYER_RADIUS:
             return True
-        
+
     return False
 
 
-
 def getRandomPipe():
-    pipe_height = PIPE_HEIGHT
-    offset = SCREEN_HEIGHT / 3
-    y2 = offset + random.randrange(0, SCREEN_HEIGHT)
+    gap = 150
     pipe_x = SCREEN_WIDTH + 10
-    y1 = pipe_height - y2 + offset
-    return [{"x": pipe_x, "y": -y1}, {"x": pipe_x, "y": y2}]
+    lower_pipe_y = random.randrange(gap + 50, SCREEN_HEIGHT - 50)
+    upper_pipe_y = lower_pipe_y - gap - PIPE_HEIGHT
+    return [{"x": pipe_x, "y": upper_pipe_y}, {"x": pipe_x, "y": lower_pipe_y}]
 
 
 if __name__ == "__main__":
     pygame.init()
     pygame.font.init()
-    my_font = pygame.font.SysFont('Comic Sans MS', 30)
+    my_font = pygame.font.SysFont("Comic Sans MS", 30)
     FPSCLOCK = pygame.time.Clock()
     pygame.display.set_caption("Flappy Bird Clone")
 
