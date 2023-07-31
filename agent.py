9ebd26b1c65e2fb37f55eeaea471ba321e544362
Siddharth <2004sid@gmail.com>
2023-07-31 12:31:57 +0530

    fine tuning the model

--- agent.py
+++ agent.py
@@ -16,7 +16,7 @@ class Agent:
         self.epsilon = 0
         self.gamma = 0.9
         self.memory = deque(maxlen=MAX_MEMORY)
-        self.model = Linear_QNet(9, 256, 1)
+        self.model = Linear_QNet(13, 256, 1)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
 
@@ -30,6 +30,10 @@ class Agent:
             game.upcoming_pipes[0]['y'] + game.pipe_height,
             game.upcoming_pipes[1]['x'],
             game.upcoming_pipes[1]['y'],
+            game.upcoming_pipes[2]['x'],
+            game.upcoming_pipes[2]['y'] + game.pipe_height,
+            game.upcoming_pipes[3]['x'],
+            game.upcoming_pipes[3]['y'],
             #distance of player y coordinates and upcoming pipe y coordinates
             game.player_y - game.upcoming_pipes[0]['y'] + game.pipe_height,
             game.player_y - game.upcoming_pipes[1]['y'],
