94f24a1fdfbc549c26ebba9fdfab23bec35d42db
Siddharth <2004sid@gmail.com>
2023-12-28 15:32:47 +0530

    chart formatting

--- helper.py
+++ helper.py
@@ -12,11 +12,12 @@ def plot(scores, mean_scores, to_save):
     plt.title('Training Metrics')
     plt.xlabel('Number of Games')
     plt.ylabel('Score')
-    plt.plot(scores)
-    plt.plot(mean_scores)
+    plt.plot(range(len(scores)), scores, marker='o', label='Scores', linestyle='-', linewidth=0.5)
+    plt.plot(range(len(mean_scores)), mean_scores, color='orange', label='Mean Scores')
     plt.ylim(ymin=0)
     plt.text(len(scores)-1, scores[-1], str(scores[-1]))
     plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    plt.legend(loc='upper right', bbox_to_anchor=(1.05, 1))
     if to_save:
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         filename = f"training_results/training_run_{timestamp}.png"
